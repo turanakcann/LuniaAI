e74ed2135f22af0a16acab06dca6dfd2bd1ff02f
turanakcann <turanakcnnofficial@gmail.com>
2026-05-27 04:59:28 +0300

    api way updated

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-// Backend adresimizi tanımlıyoruz
+// Backend adresimizi tanımlıyoruz (Artık dinamik olarak .env'den çekecek)
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
